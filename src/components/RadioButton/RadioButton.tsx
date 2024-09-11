dd474e18db2e2807b706ef5287d9001fe1b212ed
Avinash Shelke <avishelke059@gmail.com>
2024-09-11 23:21:08 +0530

    Radio component cleanup

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export interface RadioButtonProps {
   name?: string;
   value?: string;
@@ -20,38 +22,41 @@ export const RadioButton = ({
   orientation = 'vertical',
   className = '',
 }: RadioButtonProps) => {
+  const handleChange = (value: string) => {
+    onValueChange(value);
+  };
+
   return (
     <div
       role="radiogroup"
-      className={`flex  ${orientation === 'horizontal' ? 'flex-row space-x-4' : 'flex-col space-y-4'} ${className}`}
+      className={`flex ${orientation === 'horizontal' ? 'flex-row space-x-4' : 'flex-col space-y-4'} ${className}`}
     >
       {options.map((option) => (
         <label
           key={option.value}
-          htmlFor={`label-${option.value}`}
-          className="custom-radio flex items-center cursor-pointer relative pl-4"
-          onClick={() => onValueChange(option.value)}
+          htmlFor={`radio-${option.value}`}
+          className="flex items-center cursor-pointer relative pl-4"
+          onClick={() => handleChange(option.value)}
         >
           <input
             type="radio"
             name={name}
             value={option.value}
             checked={value === option.value || defaultValue === option.value}
-            aria-labelledby={`label-${option.value}`}
+            aria-labelledby={`radio-${option.value}`}
             aria-checked={value === option.value}
             className="hidden peer"
-            onChange={() => null}
           />
           <span
-            className="radio-checkmark peer-checked:bg-primary-4 peer-checked:border-primary-4 before:peer-checked:scale-100 absolute left-0 w-4 h-4 rounded-full border-2 border-neutral-4 bg-neutral-1 focus:ring-2 focus:ring-primary-4 focus:ring-offset-2 focus:border-primary-4 hover:border-primary-4 before:w-2 before:h-2 before:top-[30%] before:left-[30%] before:content-[''] before:absolute before:bg-neutral-1 before:rounded-full transition duration-75"
+            className="peer-checked:bg-primary-4 peer-checked:border-primary-4 absolute left-0 w-4 h-4 rounded-full border-2 border-neutral-4 bg-neutral-1 focus:ring-2 focus:ring-primary-4 focus:ring-offset-2 focus:border-primary-4 hover:border-primary-4 focus-visible:outline-primary-4 transition duration-75"
             tabIndex={0}
             role="radio"
             aria-checked={value === option.value}
-            id={`label-${option.value}`}
-          ></span>
-          <span className="ml-3 text-shade-4" aria-label={option.label}>
-            {option.label}
+            id={`radio-${option.value}`}
+          >
+            <span className="w-2 h-2 absolute left-[30%] top-[30%] bg-neutral-1 rounded-full peer-checked:scale-100 transition-transform duration-75" />
           </span>
+          <span className="ml-3 text-shade-4">{option.label}</span>
         </label>
       ))}
     </div>
