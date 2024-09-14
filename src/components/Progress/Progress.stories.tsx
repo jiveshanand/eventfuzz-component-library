a94ee511bbbf1188e1d118be6a5ee37c224b70ea
Avinash Shelke <avishelke059@gmail.com>
2024-09-14 14:20:33 +0530

    minor change

--- src/components/Progress/Progress.stories.tsx
+++ src/components/Progress/Progress.stories.tsx
@@ -13,7 +13,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default: Story = {
   render: (args) => {
     return <div className='w-[500px]'><Progress {...args} /></div>
   },
