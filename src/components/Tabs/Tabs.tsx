a9fb7a6bc1de916ce2afafc5a59c3dd4b6838188
Avinash Shelke <avishelke059@gmail.com>
2024-09-14 23:35:26 +0530

    Tabs test case

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -21,13 +21,15 @@ export const Tabs: React.FC<TabsProps> = React.memo(
   }: TabsProps) => {
     const [activeTab, setActiveTab] = useState(0);
 
+    // Handler for tab click
     const handleTabClick = (index: number) => {
       setActiveTab(index);
       onChange?.(index, options[index].label);
     };
 
     // Define common styles
-    const commonTabStyles = 'cursor-pointer focus:ring-2 focus:ring-primary-4';
+    const commonTabStyles =
+      'cursor-pointer focus:ring-2 focus:ring-primary-4 focus-visible:outline-none';
 
     // Variant-specific styles
     const variantStyles = {
@@ -37,24 +39,25 @@ export const Tabs: React.FC<TabsProps> = React.memo(
     }[variant];
 
     const tabStyle = {
-      pills: `w-[151px] py-2 px-2.5 flex justify-center text-sm font-semibold rounded-full hover:bg-shade-1 focus:bg-shade-1 focus:z-10 ${commonTabStyles}`,
+      pills: `w-[151px] py-2 px-2.5 flex justify-center text-sm font-semibold rounded-full hover:bg-shade-1 focus:bg-shade-1 ${commonTabStyles}`,
       outlined: `flex items-center px-3 py-2 gap-2 rounded-lg text-shade-4 border border-neutral-6 hover:bg-shade-2 focus:bg-shade-1 focus:text-primary-4 focus:ring-1 focus:ring-primary-4 focus:border-primary-4 ${commonTabStyles}`,
       default:
-        'flex flex-col gap-2 p-2 peer rounded-lg cursor-pointer focus:text-primary-4 focus:ring-2 focus:ring-primary-4',
+        'peer flex flex-col gap-2 p-2 rounded-lg cursor-pointer focus:text-primary-4 focus:ring-2 focus:ring-primary-4',
     }[variant];
 
     return (
       <div
         className={`flex items-center ${variantStyles} ${className}`}
-        role="tabs"
+        role="tablist"
       >
         {options.map((item, index) => (
-          <div className="flex flex-col gap-2" key={item.label}>
+          <div key={item.label} className="flex flex-col gap-2">
             <div
-              className={tabStyle}
+              className={`${tabStyle} ${variant === 'pills' ? (activeTab === index ? 'bg-shade-1' : '') : ''}`}
               onClick={() => handleTabClick(index)}
               tabIndex={0}
               role="tab"
+              aria-selected={activeTab === index}
             >
               {item.icon && (
                 <div className="flex justify-center">{item.icon}</div>
