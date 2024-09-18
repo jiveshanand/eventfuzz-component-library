84457fba002e4905bcca60d0a84ea378ec9b9b99
Aditi Thakur <aditithakur@Aditis-MacBook-Pro.local>
2024-09-18 16:18:55 -0700

    Button Component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,52 +1,31 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-import { Button } from './Button';
+import { Button, ButtonProps } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: 'Example/Button',
+// Define the meta configuration for Storybook
+const meta: Meta<ButtonProps> = {
+  title: 'EventFuzz Components/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+};
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
 export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
+  render: (args) => <Button size="default" disabled buttonType="primary" />,
+};
+
+export const FullWidth: Story = {
+  render: (args) => (
+    <Button
+      size="fullWidth"
+      disabled={false}
+      //icon="https://docs.material-tailwind.com/icons/google.svg"
+      buttonType="secondary"
+    />
+  ),
 };
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,44 +1,53 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { Button } from './Button';
 
-test('applies primary class when primary prop is true', () => {
-  const renderedComponent = render(
-    <Button primary={true} label="Primary Button" />,
-  );
-  const buttonElement = renderedComponent.getByText(/Primary Button/i);
-  expect(buttonElement).toHaveClass('storybook-button--primary');
-});
+// Test to ensure the button renders correctly
+describe('Button Component', () => {
+  it('renders the button with correct text', () => {
+    render(<Button size="default" disabled={false} buttonType="primary" />);
+    const buttonElement = screen.getByText('Button');
+    expect(buttonElement).toBeInTheDocument();
+  });
 
-test('applies secondary class when primary prop is false', () => {
-  const renderedComponent = render(
-    <Button primary={false} label="Secondary Button" />,
-  );
-  const buttonElement = renderedComponent.getByText(/Secondary Button/i);
-  expect(buttonElement).toHaveClass('storybook-button--secondary');
-});
+  it('renders the button with the correct type: primary', () => {
+    render(<Button size="default" disabled={false} buttonType="primary" />);
+    const buttonElement = screen.getByText('Button');
+    expect(buttonElement).toHaveClass('bg-gradient-3');
+  });
 
-test('applies custom background color', () => {
-  const renderedComponent = render(
-    <Button backgroundColor="green" label="Custom Color Button" />,
-  );
-  const buttonElement = renderedComponent.getByText(/Custom Color Button/i);
-  expect(buttonElement).toHaveStyle({ backgroundColor: 'green' });
-});
+  it('renders a disabled button when the disabled prop is true', () => {
+    render(<Button size="default" disabled={true} buttonType="primary" />);
+    const buttonElement = screen.getByText('Button');
+    expect(buttonElement).toBeDisabled();
+  });
 
-test('displays the correct label', () => {
-  const renderedComponent = render(<Button label="Test Label" />);
-  const buttonElement = renderedComponent.getByText(/Test Label/i);
-  expect(buttonElement).toBeInTheDocument();
-});
+  it('fires the onClick event when clicked', () => {
+    const handleClick = jest.fn();
+    render(
+      <Button
+        size="default"
+        disabled={false}
+        buttonType="primary"
+        onClick={handleClick}
+      />,
+    );
+    const buttonElement = screen.getByText('Button');
+    fireEvent.click(buttonElement);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
 
-test('handles click events', () => {
-  const handleClick = jest.fn();
-  const renderedComponent = render(
-    <Button onClick={handleClick} label="Click Me" />,
-  );
-  const buttonElement = renderedComponent.getByText(/Click Me/i);
-  userEvent.click(buttonElement);
-  expect(handleClick).toHaveBeenCalledTimes(1);
+  it('renders the button with an icon if provided', () => {
+    render(
+      <Button
+        size="default"
+        disabled={false}
+        buttonType="primary"
+        icon="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg"
+      />,
+    );
+    const iconElement = screen.getByAltText('metamask');
+    expect(iconElement).toBeInTheDocument();
+  });
 });
