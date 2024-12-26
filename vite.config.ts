5a6b719eb44d927e198bed602afdb8afc1acac24
Jivesh Anand <jivesh.11@gmail.com>
2024-12-25 18:17:05 -0800

    first working version with integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "MyComponentLibrary",
-      fileName: (format) => `my-component-library.${format}.js`,
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'EventFuzzComponentLibrary',
+      fileName: (format) => `eventfuzz-components-library.${format}.js`,
+      // formats: ['es', 'umd'],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', /^react\/.*/, 'react-dom', /react-dom\/.*/],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
